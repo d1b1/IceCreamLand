fb33a79bc950b5f561bb59f4a51c09016d1115f8
Stephan Smith <stephan@meenta.io>
2019-10-23 21:37:04 -0400

    Update webhoooks

--- heroku/routes/rebuild.js
+++ heroku/routes/rebuild.js
@@ -47,7 +47,7 @@ module.exports = (req, res) => {
 		// Check if we have the content type in the results.
 		if (!_.has(result.data, queryName)) {
 			console.log('Dump the query result', result);
-			return res.status(200).send(`Sorry, could not find the content type "${req.query.contentType}". (${queryName}) Check your content type again.`)
+			return res.status(200).send(`Sorry, could not find the content type. (${queryName}) Check your content type again.`)
 		}
 
 		var items = result.data[queryName].items
@@ -64,7 +64,7 @@ module.exports = (req, res) => {
 		index
 			.addObjects(list)
 			.then((data) => {
-				res.status(200).send(`Rebuild all takeshape data to the index "${req.query.contentType}", having ${data.objectIDs.length} records.`)
+				res.status(200).send(`Rebuild all takeshape data to the index", having ${data.objectIDs.length} records.`)
 			})
 			.catch(err => {
 				console.log('Got an Eror', err);
